68e3f0409af1e38fa0e13989f4ed8c3ba22ed903
Afolayan Oluwaseun Raphael <geekzoneguy@gmail.com>
2019-03-06 02:35:40 +0100

    updated app.js

--- src/js/app.js
+++ src/js/app.js
@@ -11,7 +11,23 @@ $('#searchUser').on('keyup', function(e){
             }
 
     }).done(function(user){
-        console.log(user);
+        $('#profile').html(
+            `<div class="card col s12 m8 offset-m2 l6 offset-l3">
+            <div class="card-image waves-effect waves-block waves-light">
+              <img style="width:200px" class="circle responsive-img activator" src= "${user.avatar_url}">
+            </div>
+            <div class="card-content">
+              <span class="card-title activator grey-text text-darken-4">${user.name}<i class="material-icons right">more_vert</i></span>
+              <p>Visit <a href="${user.html_url}">${user.name}'s Profile On Github</a></p>
+            </div>
+            <div class="card-reveal">
+    <span class="card-title grey-text text-darken-4">${user.name}'s Bio<i class="material-icons right">close</i></span>
+              <p>${user.bio}</p>
+            </div>
+          </div>
+                    
+
+            `)
     })
 })
 });
