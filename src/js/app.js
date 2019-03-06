31f62a9dd3859499460009eed1766fe697aecd94
Afolayan Oluwaseun Raphael <geekzoneguy@gmail.com>
2019-03-06 02:39:03 +0100

    Fixed avatar width responsiveness

--- src/js/app.js
+++ src/js/app.js
@@ -14,7 +14,7 @@ $('#searchUser').on('keyup', function(e){
         $('#profile').html(
             `<div class="card col s12 m8 offset-m2 l6 offset-l3">
             <div class="card-image waves-effect waves-block waves-light">
-              <img style="width:200px" class="circle responsive-img activator" src= "${user.avatar_url}">
+              <img style="width:100%" class="circle responsive-img activator" src= "${user.avatar_url}">
             </div>
             <div class="card-content">
               <span class="card-title activator grey-text text-darken-4">${user.name}<i class="material-icons right">more_vert</i></span>
